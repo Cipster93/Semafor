ffd1b3982559def5f3ac669a816cd589bc90d548
Cipster93 <154366238+Cipster93@users.noreply.github.com>
2023-12-27 18:52:02 +0200

    semafor.js

--- semafor.js
+++ semafor.js
@@ -2,13 +2,13 @@ var x = 1;
 function ChangeCollor(button) {
     if (x == 1) {
         button.style.background = "red";
-        x = 2;
+        ++x;
     } else if (x == 2) {
         button.style.background = "green";
-        x = 3;
+        ++x ;
     } else if (x == 3) {
         button.style.background = "yellow";
-        x = 1;
+        x -= 2;
     }
 }
 setInterval(function() {
